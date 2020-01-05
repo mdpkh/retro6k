cc420462baeed0257ffd8d0f18854bc15dc8f3ad
vtk <vidthekid@gmail.com>
2020-01-05 16:26:12 -0500

    make it also build with MSVC++

--- main.cpp
+++ main.cpp
@@ -5,7 +5,11 @@
 #include <random>
 #include <string>
 
+#ifdef _MSC_VER
+#include <SDL.h>
+#else
 #include <SDL2/SDL.h>
+#endif
 
 #include "custombuild.h"
 #include "fake6502.h"
--- strcpys.h
+++ strcpys.h
@@ -1,4 +1,13 @@
 #pragma once
+// MS Visual C++ prefers to use strcpy_s instead of strcpy, which has 
+// an extra parameter in the middle specifying the destination buffer 
+// length. But this is not part of the C++ standard,  so for non-MSVC 
+// compilers, we'll alias it to the standard function by dropping the 
+// length parameter. There is a slight difference in behavior between 
+// strcpy and strcpy_s with a source cstring shorter than the destin-
+// ation  buffer,  but that  difference doesn't  matter for  what the 
+// function is used for here.
+#ifndef _MSC_VER
 #define strcpy_s(d, l, s) strcpy(d, s)
-
+#endif
 
